34b063eada91621277c11ebae39fd3f3f973f6a4
DONAT: Developers of Next Advanced Technology <francisco.283214@ortigas-cainta.sti.edu.ph>
2024-06-15 20:27:09 +0800

    Uploaded: 09 LMS Activity 1 and 10 Performance Task 1

---
+++ MAWD401/FRANCISCO_CLARENCE_DALE/09 LMS Activity 1/Controllers/MyMVC.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMVCMovieApp.Controllers
+{
+    public class MyMVC : Controller
+    {
+        // Get: /MyMVC/Welcome
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
