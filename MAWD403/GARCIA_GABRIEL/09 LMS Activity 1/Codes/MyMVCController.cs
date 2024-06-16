52e59b29854e7afee8c059dc8da01ebe0bc7f742
Gabriel <garciagabriellopos07@gmail.com>
2024-06-16 18:24:01 +0800

    Create MyMVCController.cs

---
+++ MAWD403/GARCIA_GABRIEL/09 LMS Activity 1/Codes/MyMVCController.cs
@@ -0,0 +1,18 @@
+// MyMVCController.cs
+// This is within the 'Controllers' folder
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Garcia_WebApplication.Controllers
+{
+    public class MyMVCController : Controller
+    {
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
