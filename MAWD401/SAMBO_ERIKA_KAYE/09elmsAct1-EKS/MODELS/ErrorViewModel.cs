caf2e710c0f3a850bbcec0d269b8640c6adbfe5d
Erika Sambo <169015787+AkayeSambo7@users.noreply.github.com>
2024-06-16 16:42:38 +0800

    Delete MAWD401/SAMBO_ERIKA_KAYE/09elmsAct1-EKS/MODELS directory
    deleted

--- MAWD401/SAMBO_ERIKA_KAYE/09elmsAct1-EKS/MODELS/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _09lmsAct1_EKS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
