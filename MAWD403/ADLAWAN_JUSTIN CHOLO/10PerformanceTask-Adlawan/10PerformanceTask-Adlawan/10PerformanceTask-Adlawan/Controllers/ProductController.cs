42caf7221fd8d55aae258e936894359f4d092da5
miko <dev@mikovilla.com>
2024-06-17 02:49:13 +0800

    remove unecessary files

---
+++ MAWD403/ADLAWAN_JUSTIN CHOLO/10PerformanceTask-Adlawan/10PerformanceTask-Adlawan/10PerformanceTask-Adlawan/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using _10PerformanceTask_Adlawan.DAL;
+using _10PerformanceTask_Adlawan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _10PerformanceTask_Adlawan.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDBContext _context;
+
+        public ProductController(ProductDBContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var products = _context.tblProducts?.ToList();
+
+            List<ProductViewModel> productList = new List<ProductViewModel>();
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    var ProductViewModel = new ProductViewModel()
+                    {
+                        ProductID = product.ProductID,
+                        ProductName = product.ProductName,
+                        Brand = product.Brand,
+                        Price = product.Price
+                    };
+                    productList.Add(ProductViewModel);
+                }
+                return View(productList);
+            }
+
+            return View(productList);
+        }
+    }
+}
---
+++ MAWD403/ADLAWAN_JUSTIN CHOLO/10PerformanceTask-Adlawan/10PerformanceTask-Adlawan/10PerformanceTask-Adlawan/Migrations/20240615115608_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _10PerformanceTask_Adlawan.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
