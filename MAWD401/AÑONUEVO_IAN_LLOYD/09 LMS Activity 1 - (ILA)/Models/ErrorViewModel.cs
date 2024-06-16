7ab350a6f2f5c1fbd0fd3794fab59effd99655be
Ian Lloyd Añonuevo <165992621+Girphus@users.noreply.github.com>
2024-06-16 22:42:21 +0800

    Added my codes in 10 Performance 1 and 09 LMS Activity 1

---
+++ MAWD401/AÑONUEVO_IAN_LLOYD/09 LMS Activity 1 - (ILA)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _09_LMS_Activity_1____ILA_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
