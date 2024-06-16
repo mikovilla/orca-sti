f14fdd6264e92ccf2eb091e9c8a8e7178bc961fb
Ian Lloyd Añonuevo <165992621+Girphus@users.noreply.github.com>
2024-06-16 22:13:44 +0800

    Added my code in 10 Performance Task 1

---
+++ MAWD401/AÑONUEVO_IAN_LLOYD/Product_Inventory_ILA/Migrations/20240616112539_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_Inventory_ILA.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Brand = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
