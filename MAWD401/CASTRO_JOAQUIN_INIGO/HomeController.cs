38c908e32beb5d3581879d8ac5b5fe7684c42777
accuw <joaquininigocastro2121@gmail.com>
2024-06-17 00:22:32 +0800

    10 Performance Task 1

--- MAWD401/CASTRO_JOAQUIN_INIGO/HomeController.cs
+++ MAWD401/CASTRO_JOAQUIN_INIGO/HomeController.cs
@@ -1,37 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
 
-namespace WebApplication2.Controllers
+namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult Privacy()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult Welcome (string name, int numTimes = 1)
-        {
-            ViewBag.Message = "Hello" + name;
-            ViewBag.NumTimes = numTimes;
-
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
