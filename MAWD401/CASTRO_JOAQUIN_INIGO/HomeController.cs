d3ce69df23bc8be5e9e59821f12fa1d5111e9488
accuw <joaquininigocastro2121@gmail.com>
2024-06-17 00:17:54 +0800

    09 LMS Activity 1

---
+++ MAWD401/CASTRO_JOAQUIN_INIGO/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Welcome (string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
