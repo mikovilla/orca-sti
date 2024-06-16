f39c660c67a96c2a5b97c19a9bceac8c6cc6f37d
PepengMaluwag <dbzforever02@gmail.com>
2024-06-16 07:52:23 -0700

    Activity 09

---
+++ Views/MyMVC/MyMVC.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Views.MyMVC
+{
+    public class MyMVC : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
