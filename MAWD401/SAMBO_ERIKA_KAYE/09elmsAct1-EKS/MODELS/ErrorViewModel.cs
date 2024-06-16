9f4c247a88ccde4ba633c5534d7d76d94176030a
Erika Sambo <169015787+AkayeSambo7@users.noreply.github.com>
2024-06-16 16:38:33 +0800

    Create ErrorViewModel.cs

---
+++ MAWD401/SAMBO_ERIKA_KAYE/09elmsAct1-EKS/MODELS/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _09lmsAct1_EKS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
