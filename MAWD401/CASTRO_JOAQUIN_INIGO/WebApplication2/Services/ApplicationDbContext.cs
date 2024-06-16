8b7b5bb55765cb1f0fa1c01991a33d94fe878d98
accuw <joaquininigocastro2121@gmail.com>
2024-06-16 19:44:20 +0800

    09 LMS Activity 1

---
+++ MAWD401/CASTRO_JOAQUIN_INIGO/WebApplication2/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
