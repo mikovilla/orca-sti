9702be22f8cd755d88c0006e79a33d987196657b
RyanDanielRodriguez <baconguy098@gmail.com>
2024-06-17 19:49:20 +0800

    Create DemoParty.java

---
+++ MAWD203/DemoParty.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+class Party {
+    private int numGuests;
+
+    
+    public int getNumGuests() {
+        return numGuests;
+    }
+
+    
+    public void setNumGuests(int num) {
+        this.numGuests = num;
+    }
+
+    
+    public void showInvitation() {
+        System.out.println("Please come to my party!");
+    }
+}
+
+
+class DinnerParty extends Party {
+    private int foodChoice;
+
+    
+    public int getFoodChoice() {
+        return foodChoice;
+    }
+
+    
+    public void setFoodChoice(int choice) {
+        this.foodChoice = choice;
+    }
+
+    
+    @Override
+    public void showInvitation() {
+        System.out.println("Please come to my dinner party!");
+    }
+}
+
+public class DemoParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        DinnerParty dinnerParty = new DinnerParty();
+        
+        
+        System.out.print("Enter the number of guests for the dinner party: ");
+        int guests = scanner.nextInt();
+        dinnerParty.setNumGuests(guests);
+        
+        
+        System.out.print("Enter the menu option (1 for chicken or 2 for beef): ");
+        int menuOption = scanner.nextInt();
+        dinnerParty.setFoodChoice(menuOption);
+        
+        
+        System.out.println("\nThe dinner party has " + dinnerParty.getNumGuests() + " guests.");
+        System.out.println("Menu option " + dinnerParty.getFoodChoice() + " will be served.");
+        
+        
+        dinnerParty.showInvitation();
+        
+        scanner.close();
+    }
+}
