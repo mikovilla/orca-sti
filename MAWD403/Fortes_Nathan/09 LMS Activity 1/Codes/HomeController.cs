b682ab99931e1e6f3a6290142e1fa4c5a88fc3b2
nathanfortes <nathanfortes15@gmail.com>
2024-06-17 23:27:15 +0800

    Create HomeController.cs

---
+++ MAWD403/Fortes_Nathan/09 LMS Activity 1/Codes/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _09Activity_Fortes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _09Activity_Fortes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
