beaae7baa19c286efda0ead0b76e91c997e20e12
aeapostol <apostolaprilanne@gmail.com>
2024-06-18 05:34:51 +0800

    Add files via upload

---
+++ MAWD403/Apostol_April_Anne/10PerformanceTask_Apostol.sql
@@ -0,0 +1,18 @@
+--CREATE DATABASE ProductsApostol
+--USE ProductsApostol
+
+/*CREATE TABLE tblProductsAP(
+productID int NOT NULL,
+productName varchar(50) NOT NULL,
+Brand varchar(50) NOT NULL,
+Price float NOT NULL)*/
+
+/*INSERT INTO tblProductsAP (productID, productName, Brand, Price) 
+VALUES
+(1122, 'Solana', 'LSY', 99975.36),
+(1122, 'Selena', 'LSY', 45735.89),
+(1122, 'Amora', 'LSY', 345293.17),
+(1122, 'Natasha', 'LYS', 15845.99),
+(1122, 'Katarina', 'LYS', 789498.75);
+
+SELECT * FROM tblProductsAP*/
