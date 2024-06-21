3fa1c3957e4de99de5f8d1f2c912faf040d308be
Yuri Cruz <cruzyurijenell@gmail.com>
2024-06-21 23:17:30 +0800

    Create ApplicationDbContext.cs

---
+++ MAWD401/CRUZ_YURI/10 _PT_01/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using NewMVCWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewMVCWeb.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
