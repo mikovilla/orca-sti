77b7f2415af41c2c50e5b27085d036ae6d842a2a
Miko Villa <dev@mikovilla.com>
2024-06-22 08:00:21 +0800

    Delete WebApplication7 directory
    cleaned

--- WebApplication7/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication7.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
