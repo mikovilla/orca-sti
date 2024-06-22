cd3e24a353684f72a8e4ed692f58c08ab7b3ab5a
RhedLatorre <akosirhedashlee2005@gmail.com>
2024-06-22 13:48:53 +0800

    Add files via upload

---
+++ MAWD403/LATORRE_RHED_ASHLEE/09_ActivityLatorre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Activity9_Latorre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
